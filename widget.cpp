30381a505a8c54e410f5a1c5b499b296e3f868e9
rita <pocyriteko@gmail.com>
2023-05-17 13:28:28 +0700

    l2

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,133 @@
 #include "widget.h"
 #include "ui_widget.h"
 
-Widget::Widget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::Widget)
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);//устанавливаем сцену
+    scene = new QGraphicsScene;
+
+    ui->graphicsView->setScene(scene);
+
+    timer = new QTimer(this);//таймер
+    connect(timer,SIGNAL(timeout()),this,SLOT(Draw()));
+    timer->start(500);
+}
+
+void Widget::updateMatr()//считаем матрицы
+{
+    float arrT[] = {1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                   -x0,-y0,-z0,1};
+    float arrS[] = {-1,0,0,0,
+                    0,1,0,0,
+                    0,0,1,0,
+                    0,0,0,1};
+    float arrRx90[] = {1,0,0,0,
+                       0,0,-1,0,
+                       0,1,0,0,
+                       0,0,0,1};
+    float d = sqrt(x0*x0+y0*y0);
+    float arrRy[] = {y0/d,0,x0/d,0,
+                       0,1,0,0,
+                     -x0/d,0,y0/d,0,
+                       0,0,0,1};
+    float s = sqrt(x0*x0+y0*y0+z0*z0);
+    float arrRx[] = {1,0,0,0,
+                       0,d/s,-z0/s,0,
+                       0,z0/s,d/s,0,
+                       0,0,0,1};
+    T = Matrix(4,4,arrT);
+    S = Matrix(4,4,arrS);
+    Rx90= Matrix(4,4,arrRx90);
+    Ry = Matrix(4,4,arrRy);
+    Rx = Matrix(4,4,arrRx);
+    //сохраняем начальные вершины в NewVer
+
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            NewVer[i][j] = Ver[i][j];
+        }
+    }
+}
+
+void Widget::SKM_SKN()
+{
+  Matrix M();
+  V = T;
+  V *= S;
+  V *= Rx90;
+  V *= Ry;
+  V *= Rx;
+  //Считам матрицу(Вот тут поменялись вершины Ver, для чего и создан был массив NewVer)
+  for(int i=0;i<12;i++)
+  {
+    float M[] = {NewVer[i][0],NewVer[i][1],NewVer[i][2],1};//Тут все по формулам перемножаем
+    Matrix Matr(1,4,M);
+    Matr*=V;
+    float** new_ver = Matr.GetArr();
+    NewVer[i][0] = new_ver[0][0];//пересчитали вершины
+    NewVer[i][1] = new_ver[0][1];
+    NewVer[i][2] = new_ver[0][2];
+  }
+}
+
+void Widget::SKN_SKK()
+{
+  //перешли к картинной системе(тут использовалась парралельная проекция)
+    for(int i=0;i<12;i++)
+    {
+        VerKa[i][0] = NewVer[i][0];
+        VerKa[i][1] = NewVer[i][1];
+    }
+}
+
+void Widget::SKK_SKEi()
+{
+    for(int i =0;i<12;i++)
+    {
+        VerEk[i][0] = VerKa[i][0]/P*xe+xc;
+        VerEk[i][1] = VerKa[i][1]/P*ye+yc;
+    }
+}
+
+void Widget::print_scene()
+{
+    for(int i =0;i<15;i++)//рисуем все ребра
+    {
+        if (i!=7)
+        {
+            QLineF slide(QPointF(VerEk[Reb[i][0]][0],-VerEk[Reb[i][0]][1]),QPointF(VerEk[Reb[i][1]][0],-VerEk[Reb[i][1]][1]));
+            scene->addLine(slide);
+        }
+
+    }
+    QPen pen;
+    pen.setWidth(2);//последнее ребро рисуем жирненьким
+    QLineF slide(QPointF(VerEk[Reb[7][0]][0],-VerEk[Reb[7][0]][1]),QPointF(VerEk[Reb[7][1]][0],-VerEk[Reb[7][1]][1]));
+    scene->addLine(slide,pen);
+}
+
+
+void Widget::Draw()
+{
+    scene->clear();//очищаем сцену
+
+    updateMatr();//считаем марицы
+    SKM_SKN();
+    SKN_SKK();
+    SKK_SKEi();//переходим к нужной системе
+    print_scene();//рисуем
+    if(x0>1)//меняем точку зрения
+    {
+           x0 -= 1;
+           y0 -= 1;
+    }
+
 }
 
 Widget::~Widget()
--- widget.h
+++ widget.h
@@ -2,21 +2,60 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QGraphicsScene>
+#include "matrix.h"
+#include <QTimer>
 
-namespace Ui {
-class Widget;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
 
 class Widget : public QWidget
 {
     Q_OBJECT
+    //точка зрения
+    float x0 = 10;
+    float y0 = 7;
+    float z0 = 5;
+    //координаты центра поля и полуосей поля
+    int xc = 435;
+    int yc = 275;
+    int xe = 200;
+    int ye = 200;
+    //матрицы, нужные чтоб перейти от одной системы координат к другой
+    Matrix T;
+    Matrix S;
+    Matrix Rx90;
+    Matrix Ry;
+    Matrix Rx;
+    Matrix V;
+    //координаты вершин
+    float Ver[12][3] = {{0,0,0},{5,0,0},{0,5,0},{0,0,5},{1,-1,-1},{1,1,-1},{-1,1,-1},{-1,-1,-1},{1,-1,1},{1,1,1},{-1,1,1},{-1,-1,1}};
+    //копия координат вершин (чтобы действия производились с изначальными вершинами, а не с поменяными)
+    float NewVer[12][3] = {{0,0,0},{5,0,0},{0,5,0},{0,0,5},{1,-1,-1},{1,1,-1},{-1,1,-1},{-1,-1,-1},{1,-1,1},{1,1,1},{-1,1,1},{-1,-1,1}};
+    //массив ребер
+    int Reb[15][2] = {{0,1},{0,2},{0,3},{4,5},{4,7},{4,8},{5,6},{5,9},{6,7},{6,10},{7,11},{8,9},{8,11},{9,10},{10,11}};
+    //массивы вершин для картинной и экранной систем координат
+    float VerKa[12][2];
+    float VerEk[12][2];
+
+    int P = 4;
 
 public:
-    explicit Widget(QWidget *parent = 0);
-    ~Widget();
+    Widget(QWidget *parent = nullptr);
+    void SKM_SKN();//переход от мировой системы к системе координат наблюдателя
+    void SKN_SKK();//от наблюдателя к картинной
+    void SKK_SKEi();//от картинной к экранной
+    void print_scene();//рисуем куб
+    void updateMatr();//обновляем матрицы
+    ~Widget();//деструктор
 
 private:
     Ui::Widget *ui;
-};
+    QGraphicsScene *scene;
+    QTimer *timer;
 
+public slots:
+    void Draw();//это вызывается по таймеру, все рисуется и вращается
+};
 #endif // WIDGET_H
